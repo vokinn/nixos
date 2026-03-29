33f3ac546fb05fd3dc1b2ab6c86b8c6a8360960d
vokin <son.com>
2026-03-29 15:48:32 +0200

    init

---
+++ configuration.nix
@@ -0,0 +1,99 @@
+{ config, lib, pkgs, inputs, ... }:
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+    ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos";
+  networking.networkmanager.enable = true;
+  time.timeZone = "Europe/Amsterdam";
+
+  services.xserver.enable = false;
+  programs.niri.enable = true;
+  services.dbus.enable = true;
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+	alsa.enable = true;
+	alsa.support32Bit = true;
+    pulse.enable = true;
+	wireplumber.enable =  true;
+  };
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+
+  hardware.graphics.enable = true;
+  programs.dconf.enable = true;
+  programs.direnv = {
+	enable = true;
+	nix-direnv.enable = true;
+  };
+
+  programs.dconf.profiles.user.databases = [{
+    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+  }];
+
+  security.polkit.enable = true;
+
+  users.users.vokin = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "audio" "video" "networkmanager" ];
+    password = "kiruma";
+    packages = with pkgs; [
+      tree
+    ];
+  };
+
+  fonts.packages = [
+    pkgs.nerd-fonts.jetbrains-mono
+  ];
+
+  environment.systemPackages = with pkgs; [
+	# dev stuff
+    neovim
+	fzf
+	nixd
+    alacritty
+    zig
+    git
+    librewolf
+    vesktop
+    stow
+    fuzzel
+    swaybg
+    starship
+    zoxide
+    tmux
+    zsh
+    adwaita-qt
+    adwaita-qt6
+    gnome-themes-extra
+  ];
+
+  environment.sessionVariables = {
+	GTK_THEME = "Adwaita-dark";
+    QT_STYLE_OVERRIDE = "adwaita-dark";
+    QT_QPA_PLATFORM = "wayland";
+    GDK_BACKEND = "wayland,x11";
+    NIXOS_OZONE_WL = "1";
+  };
+
+  environment.etc."gtk-3.0/settings.ini".text = ''
+    [Settings]
+    gtk-application-prefer-dark-theme=1
+    gtk-theme-name=Adwaita-dark
+  '';
+  programs.zsh.enable = true;
+  system.stateVersion = "25.11";
+}
