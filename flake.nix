abc62b13cc2f0a849a8ae5cee661aaf50385b9b7
vokin <son.com>
2026-03-29 17:07:58 +0200

    codex

--- flake.nix
+++ flake.nix
@@ -1,16 +1,34 @@
 {
-    description = "NixOS configuration";
+  description = "NixOS configuration";
 
-inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    codex.url = "github:sadjow/codex-cli-nix";
+  };
 
-    outputs = { self, nixpkgs, ... }: {
-        nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-	    system = "x86_64-linux";
-	    modules = [
-	        ./configuration.nix
-	    ];
-	};
+  outputs =
+    {
+      self,
+      nixpkgs,
+      codex,
+      ...
+    }:
+
+    {
+      nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          (
+            { pkgs, ... }:
+            {
+              environment.systemPackages = [
+                codex.packages.x86_64-linux.default
+              ];
+            }
+          )
+
+          ./configuration.nix
+        ];
+      };
     };
 }
